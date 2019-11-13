d12598e7e8fc100cca96541055d7cc692c69c8d0
Anuraag <princeanurag2011@gmail.com>
2019-11-13 12:35:56 +0530

    updated comments

--- JenkinsSetup.sh
+++ JenkinsSetup.sh
@@ -3,7 +3,7 @@
 # 1. JDK 1.8
 # 2. MAVEN
 # 3. GIT
------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------
 # 1.	JDK 8 Setup.
 # Use the following commands
   sudo yum install -y java-1.8.0-openjdk-devel.x86_64;
@@ -12,24 +12,24 @@
 # When prompted, enter the number for Java 8
    sudo /usr/sbin/alternatives --config java;
    sudo /usr/sbin/alternatives --config javac;
------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------
 # 2.	MAVEN setup
 # Use the following commands
    sudo wget https://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo;
    sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo;
    sudo yum install -y apache-maven;
------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------
 # 3.	GIT setup
 #Use the following commands
     sudo yum install -y git;
------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------
 # 4.	Jenkins Setup:
 # Use the following commands
 
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo;
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key;
 sudo yum install -y jenkins;
------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------
 # 5.	Configure Jenkins
 # Use the following commands.
   sudo service jenkins start;
@@ -41,14 +41,14 @@ sudo yum install -y jenkins;
 # Note:
 # To get the password use the following command
 cat /var/lib/jenkins/secrets/initialAdminPassword
------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------
 # 6.	Install Jenkins Plugins
 # 1. Click on install suggested plugins. (#takes some time to install, may be 5 min..)
 # 2. Enter new Username, Password and email when asked.
 # 3. click on save and continue.
 
 That’s It Jenkins Loaded..!!
------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------
 # For Deployments: 
 # Install plugin:   Deploy to container 
 # Click on manage jenkins > click on manage plugins > click on Available > search for Deploy to container plugin  and then Install it. 	
