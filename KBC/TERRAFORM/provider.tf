4ce292115e08ecf2c7517fc92147b8cdd9e6d9dd
Anurag P <Anurag@desktop.com>
2020-02-24 19:51:01 +0530

    updated

--- KBC/TERRAFORM/provider.tf
+++ KBC/TERRAFORM/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-access_key = "AKIASQGZMYNB3NZAE3QL" #AKIASQGZMYNB2J66LOJ2
-secret_key = "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" #48dR/q2xz7nlC1XW8w/RdCauSpfF6UyUv0dQlcoM
+access_key = "AKIASQGZMYNB3QL" 
+secret_key = "MTECsCDJwkawyYj+" 
 region = "ap-south-1"
 }
